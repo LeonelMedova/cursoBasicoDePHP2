b9632260a4d1562424a588dff2ff4b04dd69007f
leonelmedova <ldavid.mdn@gmail.com>
2024-02-20 08:33:19 -0500

    Cl 11

---
+++ 11-condicional-for.php
@@ -0,0 +1,19 @@
+<?php
+
+for ($contador = 1; $contador <= 10; $contador++) { 
+    echo $contador . "\n";
+}
+
+echo "\n";
+
+for ($i = 10; $i > 0 ; $i--) { 
+    echo $i . "\n";
+}
+
+echo "\n";
+
+for ($i = 0, $j = 0; $i <= 10; $i++, $j += 2) { 
+    echo "i = $i  j = $j" . "\n";
+}
+
+echo "\n";
