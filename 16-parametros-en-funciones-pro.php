fd93f3c8e766f8a19e06453d69840442589b13ae
leonelmedova <ldavid.mdn@gmail.com>
2024-02-23 18:12:30 -0500

    Cl 16

---
+++ 16-parametros-en-funciones-pro.php
@@ -0,0 +1,43 @@
+<?php
+
+// function suma ($a = 1, $b = 1) {
+//     echo "La suma de $a + $b es: " . $a + $b . "\n";
+// }
+
+// suma(20, 20);
+
+// $arreglo1 = [1, 2, 3];
+// $arreglo2 = [4, 5, 6];
+
+// $resultado = [...$arreglo1, ...$arreglo2];
+
+// var_dump($resultado);
+
+// function suma ($a, $b) {
+//     echo "La suma de $a + $b es: " . $a + $b . "\n";
+// }
+
+// $numeros = [1, 2];
+// $numeros2 = [50, 20];
+// $numeros3 = [1000, 2];
+
+// suma(...$numeros);
+// suma(...$numeros2);
+// suma(...$numeros3);
+
+function suma_infinita(...$params) {
+
+    $suma = 0;
+
+    foreach ($params as $numero) {
+        $suma += $numero;
+    }
+
+    echo "La sumatoria es: $suma \n";
+}
+
+suma_infinita(1, 2);
+suma_infinita(1, 2, 3);
+suma_infinita(100, 40, 2, 9, 3);
+
+echo "\n";
