493f7140b0159fa078ee35b8de20cefa37fd87f9
leonelmedova <ldavid.mdn@gmail.com>
2024-02-23 17:33:07 -0500

    Cl 15

---
+++ 15-parametros-en-funciones.php
@@ -0,0 +1,22 @@
+<?php
+
+function es_estudiante_legend($platzi_rank) {
+
+    if ($platzi_rank >= 20000) {
+        echo "Eres un estudiante Legend!\n";
+    }
+    else {
+        echo "Lo sentimos, aun no alcanzas el nivel legend\n";
+    }
+}
+
+
+do {
+
+    $platzi_rank_user = (int) readline("Por favor, dinos cual es tu Platzi Rank: ");
+    
+    es_estudiante_legend($platzi_rank_user);
+    
+} while (false);  // Si lo dejamos true se repite indefinidamente
+
+echo "\n";
