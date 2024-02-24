0aa14cd35a203abd43e7291170f0043e17799e6c
leonelmedova <ldavid.mdn@gmail.com>
2024-02-23 19:23:12 -0500

    Cl 22 con error

--- 21-juego-de-ahorcado.php
+++ 21-juego-de-ahorcado.php
@@ -13,6 +13,7 @@ $word_length = strlen($choosen_word); // Cantidad de letras
 $discovered_letters = str_pad("", $word_length, "_");  // Llena con guion bajo en donde no se sabe las letras
 $attempts = 0;
 
+
 echo "Palabra de $word_length letras \n\n";
 echo $discovered_letters . "\n\n";
 
@@ -20,5 +21,20 @@ echo $discovered_letters . "\n\n";
 $player_letter = readline("Escribe una letra: ");
 $player_letter = strtolower($player_letter);
 
+if ( str_contains($choosen_word, $player_letter) ) {
+    // Verificamos todas las ocurrencias de esta letra para reemplazarla
+    $offset = 0;
+    while ( $letter_position = strpos($choosen_word, $player_letter, $offset) !== false) { //strpos devuelve la primera letra que coincida con la palabra seleccionada
+        
+        $discovered_letters[$letter_position] = $player_letter;
+        $offset = $letter_position + 1;
+
+    }
+}
+else {
+
+    $attempts++;
+    echo "Letra incorrecta. te quedan " . (MAX_ATTEMPS - $attempts) . "intentos.";
+}
 
 echo "\n";
