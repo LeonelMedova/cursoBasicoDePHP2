d860350a7e9cda42b23c5406381b9725f7153fb0
leonelmedova <ldavid.mdn@gmail.com>
2024-02-26 18:02:59 -0500

    Cl 23 enlace y muestra con HTML y PHP

--- 24-php-html.php
+++ 24-php-html.php
@@ -1,3 +1,9 @@
+<?php
+
+$zorros = ["https://randomfox.ca/images/1.jpg", "https://randomfox.ca/images/2.jpg", "https://randomfox.ca/images/3.jpg"]
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +14,10 @@
 <body>
     
     <h1>Hola! Esto es HTML desde PHP!</h1>
+
+    <?php foreach($zorros as $zorro) { ?>
+        <img src="<?php echo $zorro; ?>" >
+    <?php } ?>
 
 </body>
 </html>
